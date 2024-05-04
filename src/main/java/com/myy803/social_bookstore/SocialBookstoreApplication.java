234cdbcd0a6cf838a516f407cab682bad4a4d6cc
Lia Vergou <48662716+liaverg@users.noreply.github.com>
2024-05-04 23:34:18 +0300

    init code commit

---
+++ src/main/java/com/myy803/social_bookstore/SocialBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.myy803.social_bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialBookstoreApplication.class, args);
+    }
+
+}
