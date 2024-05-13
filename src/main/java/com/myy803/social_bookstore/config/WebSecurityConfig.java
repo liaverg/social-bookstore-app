873e0b59d1fddde7a21a56b765c8ac7448ce01d8
Lia Vergou <48662716+liaverg@users.noreply.github.com>
2024-05-14 00:58:26 +0300

    fix logout

--- src/main/java/com/myy803/social_bookstore/config/WebSecurityConfig.java
+++ src/main/java/com/myy803/social_bookstore/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.myy803.social_bookstore.config;
 
-import com.myy803.social_bookstore.domain.models.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/myy803/social_bookstore/controllers/HomepageController.java
+++ src/main/java/com/myy803/social_bookstore/controllers/HomepageController.java
@@ -3,7 +3,6 @@ package com.myy803.social_bookstore.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
