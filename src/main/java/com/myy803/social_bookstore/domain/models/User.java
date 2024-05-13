9289a964bbc230eb47b6703bb857f681b70a5998
Lia Vergou <48662716+liaverg@users.noreply.github.com>
2024-05-14 00:59:36 +0300

    fix db initialization

--- src/main/java/com/myy803/social_bookstore/domain/models/User.java
+++ src/main/java/com/myy803/social_bookstore/domain/models/User.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
     @Setter
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
-create table users (
+create table if not exists users(
     id                  bigint              not null     auto_increment,
     username            varchar(255)        unique,
     password            varchar(255)        not null,
     role                varchar(255)        not null,
     primary key (id)
-)
+);
