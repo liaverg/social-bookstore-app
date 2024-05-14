8e38dd76202dbbc79d3150a0dd9d7112727dc2f5
Lia Vergou <48662716+liaverg@users.noreply.github.com>
2024-05-15 01:30:20 +0300

    view book categories

--- src/main/java/com/myy803/social_bookstore/domain/models/BookCategory.java
+++ src/main/java/com/myy803/social_bookstore/domain/models/BookCategory.java
@@ -1,14 +1,17 @@
 package com.myy803.social_bookstore.domain.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
 @Table(name = "book_categories")
 public class BookCategory {
 
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Getter
     private String category;
 }
