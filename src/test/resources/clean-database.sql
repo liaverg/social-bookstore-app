438b0915dda4419b587bc465988a292affaa95c6
Lia Vergou <48662716+liaverg@users.noreply.github.com>
2024-05-15 23:26:17 +0300

    clean test database

--- src/test/resources/clean-database.sql
+++ src/test/resources/clean-database.sql
@@ -2,11 +2,15 @@ set foreign_key_checks = 0;
 
 truncate table users;
 truncate table user_profiles;
+truncate table authors;
+truncate table book_categories;
 truncate table user_profile_favorite_authors;
 truncate table user_profile_favorite_book_categories;
 
 alter table users auto_increment = 1;
 alter table user_profiles auto_increment = 1;
+alter table authors auto_increment = 1;
+alter table book_categories auto_increment = 1;
 alter table user_profile_favorite_authors auto_increment = 1;
 alter table user_profile_favorite_book_categories auto_increment = 1;
 
