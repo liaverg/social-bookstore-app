87ce7ff334280764375478af43c84ec515062333
Lia Vergou <48662716+liaverg@users.noreply.github.com>
2024-05-16 02:04:20 +0300

    create endpoint and html template

--- src/main/java/com/myy803/social_bookstore/config/EndpointConfig.java
+++ src/main/java/com/myy803/social_bookstore/config/EndpointConfig.java
@@ -6,4 +6,5 @@ public class EndpointConfig {
     public static final String REGISTER_PATH = "/register";
     public static final String HOMEPAGE_PATH = "/homepage";
     public static final String USER_PROFILE_PATH = "/profile";
+    public static final String BOOK_OFFERS_SAVE_PATH = "/book-offers/save";
 }
